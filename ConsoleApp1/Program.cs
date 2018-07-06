4208c1b2218dab4dc1603da6c05969a5ab7b5566
miltc <miltc@example.com>
2018-07-06 13:46:49 +0900

    new file:   ConsoleApp1/App.config 	new file:   ConsoleApp1/ConsoleApp1.csproj 	new file:   ConsoleApp1/ConsoleApp1.sln 	new file:   ConsoleApp1/Program.cs 	new file:   ConsoleApp1/Properties/AssemblyInfo.cs 	new file:   README

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Something
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var somethings = new List<Something>
+            {
+                new Something{Name = "Taro"},
+                new Something{Name = "Jiro"}
+            };
+
+            foreach (var piece in somethings)
+            {
+                Console.WriteLine(string.Format("Id={0},Name={1}", piece.Id, piece.Name));
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
